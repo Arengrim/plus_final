3f6a9830abbfc45fc2352e22348db34cfe4cbad4
Arengrim <72564064+Arengrim@users.noreply.github.com>
2020-10-30 22:34:28 +0000

    search bar temp fixed

--- src/script.js
+++ src/script.js
@@ -42,8 +42,18 @@ dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#current-weather");
   console.log(response.data)
 }
-
-  let apiKey = "ac281cbb78ff289d2f9cd6c7d0c17042";
-  let city = "Lisbon"
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
+}
+  function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#cityName");
+  search(cityInputElement.value);
+}
+search("Lisbon");
+
+let form = document.querySelector("#citySearch");
+form.addEventListener("submit", handleSubmit);
+search("Lisbon");
