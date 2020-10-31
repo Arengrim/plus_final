f54aa2d16cc7b2fa57528cb7790627449679e63b
Arengrim <72564064+Arengrim@users.noreply.github.com>
2020-10-31 12:00:08 +0000

    main icon edited

--- src/script.js
+++ src/script.js
@@ -39,14 +39,20 @@ function showTemperature(response) {
   windElement.innerHTML=response.data.wind.speed;
   let dateElement = document.querySelector("#today");
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  let iconElement = document.querySelector("#current-weather");
-  console.log(response.data)
+let iconElement=document.querySelector("#iconShow")
+iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function search(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(showTemperature);
+
 }
+  
   function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#cityName");
