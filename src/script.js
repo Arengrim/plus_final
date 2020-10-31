3467cc2896a082a0f03468fe08866355412394df
Arengrim <72564064+Arengrim@users.noreply.github.com>
2020-10-31 13:14:51 +0000

    forecast fixed

--- src/script.js
+++ src/script.js
@@ -26,6 +26,7 @@ function formatHours(timestamp) {
   }
 
   return `${hours}:${minutes}`;
+
 }
 
 function showTemperature(response) {
@@ -51,6 +52,8 @@ function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
 
 }
   
@@ -88,3 +91,28 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+function dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML= null;
+
+  let forecast= null;
+  
+for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML +=  `<div class="container">
+                    <div class="col-2">
+                        <strong> ${formatHours(forecast.dt * 1000)} </strong>
+                        </br>
+                        <div class="light">
+                            <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" id="iconShow" class="day-icon">
+                        </div>
+                        </br>
+                        <strong>${Math.round(forecast.main.temp_max)}ºC
+                        </strong>
+                    </div>
+                    
+                </div>
+            </div>`;}
+}
